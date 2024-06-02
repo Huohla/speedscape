59aefad7f16992cb3df55da8f76075534e09b742
Lorena Rojas <rojaspadronlorena@gmail.com>
2024-06-02 15:22:20 +0200

    First commit

---
+++ src/main/java/tfc/speedscape/SpeedScapeApplication.java
@@ -0,0 +1,13 @@
+package tfc.speedscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedScapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedScapeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tfc/speedscape/SpeedScapeApplicationTests.java
@@ -0,0 +1,13 @@
+package tfc.speedscape;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedScapeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
